5360d51a03235b0ccf5a8a0bfc51d332b740b23f
Chugani05 <chugani205@gmail.com>
2024-09-20 15:10:23 +0100

    reorganizando y añadiendo

---
+++ Despliegue de Aplicaciones Web/unidad 03/tareas/prueba php/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prueba PHP</title>
+</head>
+<body>
+    <form method="get" action="get_post.php">
+        Nombre : <input type="text" name="usuario">
+        <br>
+        <input type ="Submit" name="Enviar" value="Enviar">
+    </form>
+</body>
+</html>
