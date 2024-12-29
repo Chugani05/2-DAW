1817dff0bee8c91cd9cffa74240de1d4163e02c4
chugani05 <chugani205@gmail.com>
2024-12-29 21:01:12 +0000

    se termina ejercicio 2

--- Desarrollo-Web-en-Entorno-Cliente/unidad-05/tareas/tarea04/src/components/Ejercicio2.vue
+++ Desarrollo-Web-en-Entorno-Cliente/unidad-05/tareas/tarea04/src/components/Ejercicio2.vue
@@ -1,70 +1,65 @@
 <template>
-    <div class="container text-center">
-      <h2 class="mb-4">La casa flotante de los globos rojos</h2>
-      <div class="mb-3">
-        <label for="balloonSlider" class="form-label">Cantidad de Globos</label>
-        <input
-          v-model="balloonCount"
-          type="range"
-          min="1"
-          max="20"
-          id="balloonSlider"
-          class="form-range"
-        />
-        <div class="mt-2">
-          <span class="badge bg-danger">{{ balloonCount }} globos</span>
-        </div>
-      </div>
-  
-      <div class="balloon-container mb-4">
-        <div v-for="n in balloonCount" :key="n" class="balloon"></div>
-      </div>
-  
-      <button @click="revealMessage" class="btn btn-danger">
-        Mostrar Mensaje Espeluznante
-      </button>
-  
-      <div v-if="showMessage" class="mt-4">
-        <h4>¡Todos flotan aquí!</h4>
-        <p v-if="balloonCount < 10">¿Te atreves a ver más?</p>
-        <p v-else>¿Estás listo para un terror aún mayor?</p>
+  <div class="container text-center">
+    <h2 class="mb-4">La casa flotante de los globos rojos</h2>
+    <div class="mb-3">
+      <label for="balloonSlider" class="form-label">Cantidad de Globos</label>
+      <input
+        v-model="balloonCount"
+        type="range"
+        min="1"
+        max="20"
+        id="balloonSlider"
+        class="form-range"
+      />
+      <div class="mt-2">
+        <span class="badge bg-danger">{{ balloonCount }} globos</span>
       </div>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        balloonCount: 1,
-        showMessage: false
-      };
-    },
-    methods: {
-      revealMessage() {
-        if (this.balloonCount >= 10) {
-          this.showMessage = true;
-        } else {
-          alert("Necesitas al menos 10 globos para ver el mensaje.");
-        }
-      }
-    }
-  };
-  </script>
-  
-  <style scoped>
-  .balloon-container {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-    margin-top: 20px;
+
+    <div class="balloon-container mb-4">
+      <div v-for="n in balloonCount" :key="n" class="balloon"></div>
+    </div>
+
+    <button @click="showMessage = true" class="btn btn-danger">
+      Mostrar Mensaje Espeluznante
+    </button>
+
+    <div v-if="showMessage && balloonCount >= 10" class="mt-4">
+      <h4>¡Todos flotan aquí!</h4>
+      <p v-if="balloonCount >= 10 && balloonCount <= 15">
+        ¿Estás listo para un terror moderado?
+      </p>
+      <p v-else-if="balloonCount > 15">
+        ¡Cuidado! Esto puede ser demasiado espeluznante.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      balloonCount: 1,
+      showMessage: false
+    };
   }
-  
-  .balloon {
-    width: 50px;
-    height: 50px;
-    background-color: red;
-    border-radius: 50%;
-    margin: 5px;
-  }
-  </style>
+};
+</script>
+
+<style scoped>
+.balloon-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin-top: 20px;
+}
+
+.balloon {
+  width: 50px;
+  height: 50px;
+  background-color: red;
+  border-radius: 50%;
+  margin: 5px;
+}
+</style>
