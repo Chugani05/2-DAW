6853f63029f008a0a593a6f877703b226b0722f0
chugani05 <chugani205@gmail.com>
2025-01-04 00:03:08 +0000

    arreglo menor

--- Desarrollo-Web-en-Entorno-Cliente/unidad-05/tareas/tarea04/src/main.ts
+++ Desarrollo-Web-en-Entorno-Cliente/unidad-05/tareas/tarea04/src/main.ts
@@ -5,8 +5,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 import "bootstrap-icons/font/bootstrap-icons.css"
 import './style.css';
+import $ from 'jquery';
 
 
 const app = createApp(App);
+app.config.globalProperties.$ = $;
 app.use(router);
 app.mount('#app');
