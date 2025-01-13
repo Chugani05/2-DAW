457db3a45f546b2ce30f8c1058a326d27ce82a7c
Chugani05 <chugani205@gmail.com>
2025-01-13 14:53:58 +0000

    minor change

--- Desarrollo-Web-en-Entorno-Cliente/unidad-05/tareas/tarea06/src/components/NavBar.vue
+++ Desarrollo-Web-en-Entorno-Cliente/unidad-05/tareas/tarea06/src/components/NavBar.vue
@@ -11,22 +11,22 @@ function navigate(path: string) {
 </script>
 
 <template>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Auth</a>
-            <div>
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <button class="btn btn-link nav-link" @click="navigate('/shop')">Tienda</button>
-                    </li>
-                    <li class="nav-item" v-if="auth?.isAuthenticated">
-                        <button class="btn btn-link nav-link" @click="navigate('/profile')">Perfil</button>
-                    </li>
-                    <li class="nav-item" v-else>
-                        <button class="btn btn-link nav-link" @click="navigate('/login')">Login</button>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Auth</a>
+      <div>
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <button class="btn btn-link nav-link" @click="navigate('/shop')">Tienda</button>
+          </li>
+          <li class="nav-item" v-if="auth?.isAuthenticated">
+            <button class="btn btn-link nav-link" @click="navigate('/profile')">Perfil</button>
+          </li>
+          <li class="nav-item" v-else>
+            <button class="btn btn-link nav-link" @click="navigate('/login')">Login</button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
 </template>
